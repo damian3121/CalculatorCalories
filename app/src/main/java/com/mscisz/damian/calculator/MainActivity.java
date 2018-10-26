879a8c9b69182f539f3fa4e56623500c1e8719d0
damian3121 <damian.mscisz@gmail.com>
2018-10-26 16:13:16 +0200

    Add activity to entering basic data about user

--- app/src/main/java/com/mscisz/damian/calculator/MainActivity.java
+++ app/src/main/java/com/mscisz/damian/calculator/MainActivity.java
@@ -2,6 +2,9 @@ package com.mscisz.damian.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences = getSharedPreferences("preferences",MODE_PRIVATE);
+        boolean firstStart = preferences.getBoolean("firstStart", true);
+
+        if(firstStart){
+            setContentView(R.layout.activity_enter_basic_data);
+            enterBasicData();
+        }
+        else{
+            setContentView(R.layout.activity_main);
+        }
+    }
+
+    private void enterBasicData() {
+        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("firstStart", false);
+        editor.apply();
     }
 }
