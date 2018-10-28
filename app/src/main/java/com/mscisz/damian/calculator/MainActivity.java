fef1d228bdfd39f97e8a2656cf8de68672d292cd
damian3121 <damian.mscisz@gmail.com>
2018-10-28 20:52:02 +0100

    Add example menu and toggle bar

--- app/src/main/java/com/mscisz/damian/calculator/MainActivity.java
+++ app/src/main/java/com/mscisz/damian/calculator/MainActivity.java
@@ -3,36 +3,48 @@ package com.mscisz.damian.calculator;
 import android.content.Context;
 import android.content.Intent;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView input;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         SharedPreferences preferences = getSharedPreferences("preferences",MODE_PRIVATE);
         boolean firstStart = preferences.getBoolean("firstStart", true);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if(firstStart){
             Intent i=new Intent(getApplicationContext(),EnterBasicDataActivity.class);
             startActivity(i);
             enterBasicData();
         }
-        else{
-            setContentView(R.layout.activity_main);
-            input = findViewById(R.id.input);
+    }
 
-            SharedPreferences basicDataPref = getSharedPreferences("basicDataPref", Context.MODE_PRIVATE);
-            String txt = basicDataPref.getString("inputDateBirth","unkown");
-            input.setText(txt);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 
     private void enterBasicData() {
